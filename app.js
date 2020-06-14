385326addc33834f253784a50ae309aeb15189c2
HAArain <58850794+HAArain@users.noreply.github.com>
2020-06-14 07:53:56 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,285 @@
+////////////////////chapter1///////////////////
+
+////////TASK1////////
+
+// alert("assalam alikum!");
+
+
+////////TASK2////////
+
+// alert("Errot!  please enter a valid pasword");
+
+////////TASK3////////
+
+
+// alert("Welcome to JS Land  \n Happy Coding!");
+
+
+////////TASK4////////
+
+// alert("Welcome to JS Land ");
+
+// alert("Happy Coding!");
+
+
+////////TASK5////////
+
+
+// alert("Hello... i can run JS through my web browser's console");
+
+////////TASK6////////
+
+
+// <!-- <script>
+// alert("hello");
+// </script> -->
+// </head>
+// <body>
+
+// <!-- <script>
+// alert("hello");
+// </script> -->
+
+// </body>
+// </html>
+
+
+// <!-- <script>
+// alert("hello");
+// </script> -->
+
+
+
+////////////////CHAPTER 2 //////////////////
+
+
+///////////////TASK1///////////////////
+
+// var username =  "";
+
+///////////////TASK2///////////////////
+
+// var myName = "my name is hamza ahmed arain";
+
+
+///////////////TASK3//////////////////
+
+///////////////PART A//////////////////
+
+// var titled;
+
+///////////////PART B//////////////////
+
+// titled = "Hello World";
+
+// alert(titled);
+
+///////////////TASK4//////////////////
+
+// var  name  = "hamza ahmed ";
+// alert(name);
+// var age = "23 years old";
+// alert(age);
+// var education = "undergraduate";
+// alert(education);
+
+
+
+
+///////////////TASK5//////////////////
+
+
+// var a = "PIZZA\nPIZZ\nPIZ\nPI\np";
+// alert(a);
+
+
+
+///////////////TASK6//////////////////
+
+// var email = "hamza.smiu.edu.pk@gmail.com";
+// alert(email);
+
+
+///////////////TASK7//////////////////
+
+
+// var book = "i am trying to learn from the  book \n a smarter way to learn javascript";
+// alert(book);
+
+
+///////////////TASK8//////////////////
+
+// var para = "yah! i can write HTML content through JavaScript";
+// document.write(para);
+
+
+
+// var string = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(string);
+
+
+
+////////////////////CHAPTER 3 ///////////////////
+
+
+////////////TASK 1/////////////////////////////
+
+// var age = 15;
+
+// alert("i am " +age+ "year old");
+
+
+
+////////////TASK 2/////////////////////////////
+
+// var visit;
+
+// visit = 14;
+
+
+// alert("you have visited this side" +  visit  +  "time");
+
+
+////////////TASK 3/////////////////////////////
+
+// var birthyear;
+
+// birthyear = 1997;
+
+// document.write("my birth year" + birthyear);
+
+
+////////////TASK 4/////////////////////////////
+
+// var name     = "hamza ahmed arain order by";
+// var product  = "aerosoft";
+// var quantity = "5 product";
+// document.write(name + "  " + product + "  " + quantity);
+
+
+///////////////////CHAPTER5//////////////////
+
+//////////////////TASK1////////////////
+
+
+// var a,b,c;
+
+
+
+//////////////////TASK2////////////////
+
+//////////LEGAL VAR ////////////
+
+// var $hamza;
+// var hamza$;
+// var hamz89a;
+// var javascript;
+// var wo1rld;
+
+//////////ELEGAL VAR ////////////
+
+// var 1hamza;
+// var -hamza;
+// var +javascript;
+// var ja-varsc;
+// var 0netwo;
+
+
+
+
+///////////////////CHAPTER 5 //////////////////////////
+
+
+///////////////////TASK1////////////////////////////
+
+
+// var a = 3;
+// var b = 5;
+
+// var c = a+b;
+
+
+// document.write("sum of 3 and 5 is "  + c);
+
+///////////////////TASK2////////////////////////////
+
+// var a = 4;
+// var b= 2;
+// var add = a+b;
+// var sub = a-b;
+// var mul = a*b;
+// var div = a/b;
+// var mod = a%b;
+// document.write("ther first number is " + a + "<br>");
+// document.write("the second number is " + b + "<br>");
+// document.write("the additon is " + add + "<br>");
+// document.write("the subtraction is " + sub + "<br>");
+// document.write("the multiplication  is " + mul + "<br>");
+// document.write("the division is " + div + "<br>");
+// document.write("the modulus is " + mod + "<br>");
+
+
+
+
+
+///////////////////TASK3////////////////////////////
+
+////////////////PART A B //////////////
+// var a ;
+// document.write("the value after declartion is " +a);
+
+
+////////////////PART C D //////////////
+
+// var a = 7;
+
+// document.write("the inintial value is" + a);
+
+////////////////PART E  F//////////////
+
+// var a = 7;
+// var a = ++a;
+// document.write("after in cremment" + a);
+
+////////////////PART g H//////////////
+
+// var a=7;
+// var a = ++a;
+// var a = a + 7;
+// document.write("after addition " + a);
+
+////////////////PART i J//////////////
+
+// var a=7;
+// var a = --a;
+// document.write("after addition " + a);
+
+////////////////PART  K//////////////
+
+// var a=12;
+// var b=6;
+// var c= a%6;
+// document.write("remainder" + c);
+
+
+
+
+
+////////////////////TASK 4 /////////////////////////////////
+// var cost = 600;
+
+// var b = cost*5;
+
+// document.write("toal cost TO  buy 5 tickess tO a movie  " + b+"PKR");
+
+////////////////////TASK 5 /////////////////////////////////
+
+// for(i=1; i<=10; i++)
+// {
+
+//     document.write(" 4 X " + i + " = " + 4 * i + "<br>")
+// }
+
+
+
+
